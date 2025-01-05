b8e09accabca25e815658f6fed9a122951a6f5aa
Ezenweani Chukeuka Alexander (Alexone) <alexanderalexone4@gmail.com>
2025-01-05 01:19:33 +0100

    day04 commit

---
+++ daily-learning/January/day-04/exercise/day-04.js
@@ -0,0 +1,3 @@
+
+const scriptingLanguage = ("A scripting language is a programming language designed to automate tasks, control software, or enable dynamic functionality within applications. Unlike traditional programming languages, scripting languages are typically interpreted rather than compiled, meaning the code is executed line-by-line at runtime. This makes them lightweight and ideal for tasks that require quick execution.");
+console.log(scriptingLanguage);
