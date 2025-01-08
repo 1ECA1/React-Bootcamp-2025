c20474cecdf92603daca3b9c43f48e71a6572ca0
Ezenweani Chukeuka Alexander (Alexone) <alexanderalexone4@gmail.com>
2025-01-08 10:58:34 +0100

    day-07 commit

---
+++ daily-learning/January/day-07/exercise/day-07.js
@@ -0,0 +1,125 @@
+let firstName = "Ezenweani";
+console.log(firstName);
+
+let lastName = "chukwuka";
+console.log(lastName);
+
+let age = 24;
+console.log(age);
+
+let favoriteColor = "blue"
+console.log(favoriteColor);
+
+let todayDate = "7 January 2025";
+console.log(todayDate);
+
+
+let birthYear = 2002;
+console.log(birthYear);
+
+let likesChocolate = false;
+console.log(likesChocolate);
+
+let schoolName = "Unversity Of Abuja";
+console.log(schoolName);
+
+let favoriteMovie = "Inception";
+console.log(favoriteMovie);
+
+let country = "Nigeria";
+console.log(country);
+
+
+let number1 = 15;
+let number2 = 30;
+let sum = number1 + number2;
+console.log(sum);
+
+
+let length = 10;
+let width = 5;
+let area = length * width;
+console.log( area);
+
+let ageInFiveYears = age + 5;
+console.log(age);
+
+
+let height = 1.75;
+console.log(height);
+
+
+
+let sideLength = 4;
+let perimeter = 4 * sideLength;
+console.log(perimeter);
+
+
+
+let distance = 100; // in km
+let time = 2; // in hours
+let speed = distance / time;
+console.log(speed);
+
+
+
+let price = 50;
+let quantity = 3;
+let totalCost = price * quantity;
+console.log(totalCost);
+
+
+let radius = 7;
+let circumference = 2 * Math.PI * radius;
+console.log(square);
+
+
+let numberToSquare = 8;
+let square = numberToSquare ** 2;
+console.log(square);
+
+let hoursWorked = 40;
+let hourlyPay = 20;
+let salary = hoursWorked * hourlyPay;
+console.log(salary);
+
+
+
+let fullName = (`'${firstName}' + " " + '${lastName}`);
+console.log(fullName);
+
+
+let introduction = (`Hello, my name is ${fullName}, and I am ${age} years old.`);
+
+
+let concatenationSentence = "My name is " + fullName + " and I like " + favoriteColor + ".";
+
+let city = ("Abuja");
+let state = "F.C.T";
+console.log(city + " " + F.C.T);
+
+
+let hobby = "cooking";
+console.log(`i love ${hobby}`);
+
+
+console.log = (`${fullName} is ${age} years old and loves ${hobby}.;
+`)
+
+
+let message = "Hope you have a great day!";
+let greetingMessage = (`Hello! ${message}`);
+console.log(greetingMessage);
+
+let itemName = "Notebook";
+let itemPrice = 500;
+console.log(`${productName}, Price: ₦${productPrice}`);
+
+
+let quote = "Belive you can and you are half way there.";
+let author = "Theodore Roosevelt";
+console.log = (`"${quote}" + " " + ${author}`);
+
+
+let hisName = ("Joy");
+console.log = (" Hi ${friendName}, welcome to my home!");
