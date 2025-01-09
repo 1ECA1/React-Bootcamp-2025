3cb6669d2795f3000dc8e034b9e719eb8169c68a
Ezenweani Chukeuka Alexander (Alexone) <alexanderalexone4@gmail.com>
2025-01-09 01:22:57 +0100

    day-08 commit

---
+++ daily-learning/January/day-08/exercise/day-08.js
@@ -0,0 +1,119 @@
+let greeting = ("hello world");
+console.log(greeting);
+
+let name = "Ezenweani Chukwuka";
+console.log(name);
+
+let age = 24;
+console.log(age);
+
+let favoriteColor = "blue";
+console.log(favoriteColor);
+
+let todayDate = "8 January 2025";
+console.log(todayDate);
+
+let birthYear = 2002;
+console.log(birthYear);
+
+const likesChocolate = false;
+console.log(likesChocolate);
+
+let schoolName = "University Of Abuja";
+console.log(schoolName);
+
+let favoriteMovie = "Inception";
+console.log(favoriteMovie);
+
+const country = "Nigeria";
+console.log(country);
+
+const footballPosition = "goalkeeper";
+console.log(footballPosition);
+
+// number
+const studenNumber = 50;
+const teacherNumber = 5;
+const totalNumber = studenNumber + teacherNumber;
+console.log(totalNumber);
+
+const length = 10;
+const width = 5;
+const area = length * width;
+console.log(area);
+
+let acountBalance = 200;
+let price = 50;
+let remainingBalance = acountBalance - price;
+console.log(remainingBalance);
+
+
+const featureBenzz = 10;
+console.log(featureBenzz);
+
+const featureAccoutBalance = "999.99 billion dollars ++";
+console.log(featureAccoutBalance);
+
+const height = 1.75;
+console.log(height);
+
+const sideLength = 6;
+const perimeter = 4 * sideLength;
+console.log(perimeter);
+
+
+// Boolean
+const isRaining = false;
+console.log(isRaining);
+
+const isSunny = true;
+console.log(isSunny);
+
+const isCold = false;
+console.log(isCold);
+
+const myFavoritecolor = "red";
+console.log(myFavoritecolor);
+
+const myFavoriteFood = "Rice";
+console.log(myFavoriteFood);
+
+const myFavoriteSport = "Football";
+console.log(myFavoriteSport);
+
+const myFavoriteMovie = "Inception";
+console.log(myFavoriteMovie);
+
+const num = 5 > 10;
+console.log(num);
+
+const num1 = 5 < 10;
+console.log(num1);
+
+const num2 = 5 != 10;
+console.log(num2);
+
+
+// undefined
+let x;
+console.log(x);
+
+let y;
+console.log(y);
+
+let z;
+console.log(z);
+
+let a;
+console.log(a);
+
+let b;
+console.log(b);
+
+let c;
+console.log(c);
+
+let d;
+console.log(d);
+
+let e;
