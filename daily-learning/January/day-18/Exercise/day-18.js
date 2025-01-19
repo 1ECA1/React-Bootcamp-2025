52b38c516fb30b4f8f2af4b541e6822495360d12
Ezenweani Chukeuka Alexander (Alexone) <alexanderalexone4@gmail.com>
2025-01-19 22:35:51 +0100

    day-18 commit

---
+++ daily-learning/January/day-18/Exercise/day-18.js
@@ -0,0 +1,205 @@
+// create 50 arrow function and a return back the value of the function
+
+// 1. 
+const add = (a, b) => { return a + b; };
+console.log(add(2, 3)); // 5
+
+// 2. 
+const subtract = (a, b) => { return a - b; };
+console.log(subtract(5, 3)); // 2
+
+
+// 3. 
+const multiply = (a, b) => { return a * b; };
+console.log(multiply(2, 3)); // 6
+
+
+// 4. 
+const divide = (a, b) => { return a / b; };
+console.log(divide(6, 3)); // 2
+
+// 5. 
+const modulus = (a, b) => { return a % b; };
+console.log(modulus(7, 3)); // 1
+
+// 6. 
+const square = (x) => { return x * x; };
+console.log(square(3)); // 9
+
+
+// 7. 
+const cube = (x) => { return x ** 3; };
+console.log(cube(3)); // 27
+
+// 8. 
+const sqrt = (x) => { return Math.sqrt(x); };
+console.log(sqrt(81)); // 9
+
+
+// 9. 
+const abs = (x) => { return Math.abs(x); };
+console.log(abs(-5)); // 5
+
+// 10. 
+const max = (a, b) => { return Math.max(a, b); };
+console.log(max(5, 3)); // 5
+
+// 11. 
+const min = (a, b) => { return Math.min(a, b); };
+console.log(min(5, 3)); // 3
+
+// 12. 
+const celsiusToFahrenheit = (c) => { return (c * 9/5) + 32; };
+console.log(celsiusToFahrenheit(0)); // 32
+
+// 13. 
+const fahrenheitToCelsius = (f) => { return (f - 32) * 5/9; };
+console.log(fahrenheitToCelsius(32)); // 0
+
+
+// 14. 
+const isEven = (x) => { return x % 2 === 0; };
+console.log(isEven(4)); // true
+
+// 15. 
+function isOdd(x) { return x % 2 !== 0; }
+console.log(isOdd(5)); // true
+
+// 16. 
+const stringLength = (str) => { return str.length; };
+console.log(stringLength('Hello')); // 5
+
+// 17. 
+const toUpperCase = (str) => { return str.toUpperCase(); };
+console.log(toUpperCase('hello')); // HELLO
+
+// 18. 
+const toLowerCase = (str) => { return str.toLowerCase(); };
+console.log(toLowerCase('HELLO')); // hello
+
+// 19. 
+const firstChar = (str) => { return str.charAt(0); };
+console.log(firstChar('Hello')); // H
+
+// 20. 
+const lastChar = (str) => { return str.charAt(str.length - 1); };
+console.log(lastChar('Hello')); // o
+
+// 21. 
+const concatenate = (str1, str2) => { return str1 + str2; };
+console.log(concatenate('Hello', 'World')); // HelloWorld
+
+// 22.
+const arrowTwentyTwo = (num) => `This is arrow function number ${num}`;
+console.log(arrowTwentyTwo(22));
+// 23.
+const arrowTwentyThree = (num) => `This is arrow function number ${num}`;
+console.log(arrowTwentyThree(23));
+
+// 24.
+const arrowTwentyFour = (num) => `This is arrow function number ${num}`;
+console.log(arrowTwentyFour(24));
+
+// 25.
+const arrowTwentyFive = (num) => `This is arrow function number ${num}`;
+console.log(arrowTwentyFive(25));
+
+// 26.
+const arrowTwentySix = (num) => `This is arrow function number ${num}`;
+console.log(arrowTwentySix(26));
+
+// 27.
+const arrowTwentySeven = (num) => `This is arrow function number ${num}`;
+console.log(arrowTwentySeven(27));
+
+// 28.
+const arrowTwentyEight = (num) => `This is arrow function number ${num}`;
+console.log(arrowTwentyEight(28));
+
+// 29.
+const arrowTwentyNine = (num) => `This is arrow function number ${num}`;
+console.log(arrowTwentyNine(29));
+
+// 30.
+const arrowThirty = (num) => `This is arrow function number ${num}`;
+console.log(arrowThirty(30));
+
+// 31.
+const arrowThirtyOne = (num) => `This is arrow function number ${num}`;
+console.log(arrowThirtyOne(31));
+
+// 32.
+const arrowThirtyTwo = (num) => `This is arrow function number ${num}`;
+console.log(arrowThirtyTwo(32));
+
+// 33.
+const arrowThirtyThree = (num) => `This is arrow function number ${num}`;
+console.log(arrowThirtyThree(33));
+
+// 34.
+const arrowThirtyFour = (num) => `This is arrow function number ${num}`;
+console.log(arrowThirtyFour(34));
+
+// 35.
+const arrowThirtyFive = (num) => `This is arrow function number ${num}`;
+console.log(arrowThirtyFive(35));
+
+// 36.
+const arrowThirtySix = (num) => `This is arrow function number ${num}`;
+console.log(arrowThirtySix(36));
+
+// 37.
+const arrowThirtySeven = (num) => `This is arrow function number ${num}`;
+console.log(arrowThirtySeven(37));
+
+// 38.
+const arrowThirtyEight = (num) => `This is arrow function number ${num}`;
+console.log(arrowThirtyEight(38));
+
+// 39.
+const arrowThirtyNine = (num) => `This is arrow function number ${num}`;
+console.log(arrowThirtyNine(39));
+
+// 40.
+const arrowForty = (num) => `This is arrow function number ${num}`;
+console.log(arrowForty(40));
+
+// 41.
+const arrowFortyOne = (num) => `This is arrow function number ${num}`;
+console.log(arrowFortyOne(41));
+
+// 42.
+const arrowFortyTwo = (num) => `This is arrow function number ${num}`;
+console.log(arrowFortyTwo(42));
+
+// 43.
+const arrowFortyThree = (num) => `This is arrow function number ${num}`;
+console.log(arrowFortyThree(43));
+
+// 44.
+const arrowFortyFour = (num) => `This is arrow function number ${num}`;
+console.log(arrowFortyFour(44));
+
+// 45.
+const arrowFortyFive = (num) => `This is arrow function number ${num}`;
+console.log(arrowFortyFive(45));
+
+// 46.
+const arrowFortysix = (num) => `This is arrow function number ${num}`;
+console.log(arrowFortysix(46));
+
+// 47.
+const arrowFortySeven = (num) => `This is arrow function number ${num}`;
+console.log(arrowFortySeven(47));
+
+// 48.
+const arrowFortyEight = (num) => `This is arrow function number ${num}`;
+console.log(arrowFortyEight(48));
+
+// 49.
+const arrowFortyNine = (num) => `This is arrow function number ${num}`;
+console.log(arrowFortyNine(49));
+
+// 50.
+const  arrowFity = (num) => `This is arrow function number ${num}`;
+console.log(arrowFity(50));
