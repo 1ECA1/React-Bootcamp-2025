f6445082aac7d3d32d7c500958310ce34d8a0eee
Ezenweani Chukeuka Alexander (Alexone) <alexanderalexone4@gmail.com>
2025-01-23 04:15:36 +0100

    day-22 commit

---
+++ daily-learning/January/day-22/Exercise/day-22.js
@@ -0,0 +1,17 @@
+// project overview 
+
+            //Team gamma
+
+//  To create a 100 data set type for remedies and symptoms
+
+// Day 23 data creation
+// 3rd day verification
+
+//  Basic inpute handlling
+
+ ;   // git clone  repo url
+    // git add .
+    // git commit
+    // create branch
+
+    // git push origin branch Gamma
