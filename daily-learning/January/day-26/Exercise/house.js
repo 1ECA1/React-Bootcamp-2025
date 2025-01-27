a37b222e0c7a397ecb84a31ffde281642c5ee248
Ezenweani Chukeuka Alexander (Alexone) <alexanderalexone4@gmail.com>
2025-01-27 18:35:07 +0100

    day-26 commit

---
+++ daily-learning/January/day-26/Exercise/house.js
@@ -0,0 +1,11 @@
+const houses = {
+    houseOne: {
+        name: 'House Stark',
+        discription: `These topics could be woven together with real-world examples, interviews, and actionable insights to create a book that inspires and equips readers to approach business with the same rigor, passion, and determination as an Olympic athlete.`,
+        image: './day-26/exercise/asset/houseOne.jpg'
+    }
+}
+
+console.log(houses.houseOne.name);
+console.log(houses.houseOne.discription);
+console.log(houses.houseOne.image);
