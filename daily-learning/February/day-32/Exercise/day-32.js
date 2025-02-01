996920f60fa1a6b1692e984d4e640cc8cd9b92db
Ezenweani Chukeuka Alexander (Alexone) <alexanderalexone4@gmail.com>
2025-02-01 20:22:38 +0100

    day-32 commit

---
+++ daily-learning/February/day-32/Exercise/day-32.js
@@ -0,0 +1,15 @@
+const exponentialValue = Math.exp(2);
+console.log(exponentialValue);
+
+const logarithmicValue = Math.log(2);
+console.log(logarithmicValue);
+
+// Classwork: 
+    // 1 Find the value of the expression above
+    // 2 convert the floating point values of the result from the expression into a whole number
+
+    const wholeNumber = Math.round(exponentialValue);
+    console.log(wholeNumber);
+
+    const wholeNumber2 = Math.round(logarithmicValue);
+    console.log(wholeNumber2);
