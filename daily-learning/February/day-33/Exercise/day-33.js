1174a995dd186a9875e2d48fba6f56afc6140a2a
Ezenweani Chukeuka Alexander (Alexone) <alexanderalexone4@gmail.com>
2025-02-04 18:06:40 +0100

    day-33 commit

---
+++ daily-learning/February/day-33/Exercise/day-33.js
@@ -0,0 +1,22 @@
+// Day 33 Task
+
+// Create 3 variables and assign floating point numbers to each 
+
+// Convert the result to a whole number 
+
+// Create an array and assign the result to it 
+
+
+// 2: Create a variable and assign the value to a random whole number between 1 and 6
+
+const number1 = 4.9;
+const number2 = 3.2;
+const number3 = 8.5;
+
+const wholeNumber1 = Math.round(number1);
+const wholeNumber2 = Math.round(number2);
+const wholeNumber3 = Math.round(number3);
+
+const resultInArrsy = [wholeNumber1, wholeNumber2, wholeNumber3];
+
+// console.log(resultInArrsy); output (5, 3 ,6)
